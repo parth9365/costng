d2acef192327ff5260e2e3d15134bb521366190e
parth <parth.9365@gmail.com>
2019-01-23 16:50:55 +0530

    little more formatting

--- src/APIHelper.java
+++ src/APIHelper.java
@@ -16,31 +16,31 @@ public class APIHelper {
 			System.out.println(jsonArray.get(i));
 
 			String imageurl = jsonArray.getJSONObject(i).getString("jetpack_featured_media_url");
-			System.out.println("\n\n-----------------------------<image>-----------------------------");
+			System.out.println("\n-----------------------------<image>-----------------------------");
 			System.out.println(imageurl);
-			System.out.println("-----------------------------</image>-----------------------------\n\n");
+			System.out.println("-----------------------------</image>-----------------------------\n");
 			
 			String title = jsonArray.getJSONObject(i).getJSONObject("title").getString("rendered");
-			System.out.println("\n\n-----------------------------<title>-----------------------------");
+			System.out.println("\n-----------------------------<title>-----------------------------");
 			System.out.println(title);
-			System.out.println("-----------------------------</title>-----------------------------\n\n");
+			System.out.println("-----------------------------</title>-----------------------------\n");
 			
 			String content = Jsoup.parse(jsonArray.getJSONObject(i).getJSONObject("content").getString("rendered")).text();
-			System.out.println("\n\n-----------------------------<content>-----------------------------");
+			System.out.println("\n-----------------------------<content>-----------------------------");
 			System.out.println(content);
-			System.out.println("-----------------------------</content>-----------------------------\n\n");
+			System.out.println("-----------------------------</content>-----------------------------\n");
 			
 			String timestamp = jsonArray.getJSONObject(i).getString("date");
-			System.out.println("\n\n-----------------------------<timestamp>-----------------------------");
+			System.out.println("\n-----------------------------<timestamp>-----------------------------");
 			System.out.println(timestamp);
-			System.out.println("-----------------------------</timestamp>-----------------------------\n\n");
+			System.out.println("-----------------------------</timestamp>-----------------------------\n");
 			
 			Integer authorId = jsonArray.getJSONObject(i).getInt("author");
 			JSONObject authorJson = JsonReader.readJsonFromUrl("http://costng.com/wp-json/wp/v2/users/"+ authorId);
 			String author = authorJson.getString("name");
-			System.out.println("\n\n-----------------------------<author>-----------------------------");
+			System.out.println("\n-----------------------------<author>-----------------------------");
 			System.out.println(author);
-			System.out.println("-----------------------------</author>-----------------------------\n\n");
+			System.out.println("-----------------------------</author>-----------------------------\n");
 			
 			
 		}
